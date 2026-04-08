f2f3adc8f0964280a22c0b34c4e3fb462965ba51
nadznyu <nadiaalya0107@gmail.com>
2026-04-08 08:54:04 +0700

    Sequential Search (class Mahasiswa16)

---
+++ Mahasiswa16.java
@@ -0,0 +1,24 @@
+public class Mahasiswa16 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    Mahasiswa16() {
+
+    }
+
+    Mahasiswa16(String nm, String name, String kls, double ip) {
+        nim = nm;
+        nama = name;
+        ipk = ip;
+        kelas = kls;
+    }
+
+    void tampilInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK: " + ipk);
+    }
+}

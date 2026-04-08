e8b46854ce347177a94efc8a7ad4f31564426128
nadznyu <nadiaalya0107@gmail.com>
2026-04-08 08:55:29 +0700

    Sequential Search (class MahasiswaBerprestasi16)

---
+++ MahasiswaBerprestasi16.java
@@ -0,0 +1,48 @@
+public class MahasiswaBerprestasi16 {
+    Mahasiswa16[] listMhs = new Mahasiswa16[5];
+    int idx;
+
+    void tambah(Mahasiswa16 m) {
+        if (idx < listMhs.length) {
+            listMhs[idx] = m;
+            idx++;
+        } else {
+            System.out.println("data sudah penuh");
+        }
+    }
+
+    void tampil() {
+        for (Mahasiswa16 m : listMhs) {
+            m.tampilInformasi();
+            System.out.println("-----------------------");
+        }
+    }
+
+    int sequentialSearching(double cari) {
+        int posisi = -1;
+        for (int j = 0; j < listMhs.length; j++) {
+            if (listMhs[j].ipk == cari) {
+                posisi = j;
+                break;
+            }
+        }
+        return posisi;
+    }
+
+    void tampilPosisi(double x, int pos) {
+        if (pos != -1) {
+            System.out.println("data mahasiswa dengan IPK : " + x + " ditemukan pada indeks " + pos);
+        } else {
+            System.out.println("data " + x + " tidak ditemukan");
+        }
+    }
+
+    void tampilDataSearch(double x, int pos) {
+        if (pos != -1) {
+            System.out.println("nim\t : " + listMhs[pos].nim);
+            System.out.println("nama\t : " + listMhs[pos].nama);
+            System.out.println("kelas\t : " + listMhs[pos].kelas);
+            System.out.println("ipk\t : " + x);
+        }
+    }
+}

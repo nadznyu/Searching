5d5423f4a1acfac3dc8e1afd2e9aecbfc294216c
nadznyu <nadiaalya0107@gmail.com>
2026-04-08 08:56:04 +0700

    Sequential Search (class MahasiswaDemo16)

---
+++ MahasiswaDemo16.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class MahasiswaDemo16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MahasiswaBerprestasi16 list = new MahasiswaBerprestasi16();
+        int jumMhs = 5;
+
+        for (int i = 0; i < jumMhs; i++) {
+            System.out.println("Masukkan Data Mahasiswa ke-" + (i+1));
+            System.out.print("NIM     : ");
+            String nim = sc.nextLine();
+            System.out.print("Nama    : ");
+            String nama = sc.nextLine();
+            System.out.print("Kelas   : ");
+            String kelas = sc.nextLine();
+            System.out.print("IPK     : ");
+            double ipk = sc.nextDouble();
+            sc.nextLine();
+            System.out.println("----------------------------");
+
+            list.tambah(new Mahasiswa16(nim, nama, kelas, ipk));
+        }
+        list.tampil();
+        //melakukan pencarian data sequential
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Pencarian Data");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("masukkan ipk mahasiswa yang dicari: ");
+        System.out.print("IPK : ");
+        double cari = sc.nextDouble();
+
+        System.out.println("menggunakan sequential searching");
+        double posisi = list.sequentialSearching(cari);
+        int pss = (int)posisi;
+        list.tampilPosisi(cari, pss);
+        list.tampilDataSearch(cari, pss);
+        
+    }
+}

69f133e918462a1a1be9f7ac669d08ac6e52e034
nadznyu <nadiaalya0107@gmail.com>
2026-04-08 09:26:50 +0700

    Binary Search

--- MahasiswaDemo16.java
+++ MahasiswaDemo16.java
@@ -9,7 +9,7 @@ public class MahasiswaDemo16 {
         int jumMhs = 5;
 
         for (int i = 0; i < jumMhs; i++) {
-            System.out.println("Masukkan Data Mahasiswa ke-" + (i+1));
+            System.out.println("Masukkan Data Mahasiswa ke-" + (i + 1));
             System.out.print("NIM     : ");
             String nim = sc.nextLine();
             System.out.print("Nama    : ");
@@ -24,19 +24,19 @@ public class MahasiswaDemo16 {
             list.tambah(new Mahasiswa16(nim, nama, kelas, ipk));
         }
         list.tampil();
-        //melakukan pencarian data sequential
+        // melakukan pencarian data binary
         System.out.println("---------------------------------------------------------");
         System.out.println("Pencarian Data");
         System.out.println("---------------------------------------------------------");
         System.out.println("masukkan ipk mahasiswa yang dicari: ");
         System.out.print("IPK : ");
         double cari = sc.nextDouble();
-
-        System.out.println("menggunakan sequential searching");
-        double posisi = list.sequentialSearching(cari);
-        int pss = (int)posisi;
-        list.tampilPosisi(cari, pss);
-        list.tampilDataSearch(cari, pss);
-        
+        System.out.println("-------------------------");
+        System.out.println("menggunakan binary search");
+        System.out.println("-------------------------");
+        double posisi2 = list.findBinarySearch(cari, 0, jumMhs-1);
+        int pss2 = (int) posisi2;
+        list.tampilPosisi(cari, pss2);
+        list.tampilDataSearch(cari, pss2);
     }
 }

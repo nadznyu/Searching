84a0a7efa8da6311e4ddd6c6f1311f70a6acb923
nadznyu <nadiaalya0107@gmail.com>
2026-04-08 09:26:27 +0700

    Binary Search

--- MahasiswaBerprestasi16.java
+++ MahasiswaBerprestasi16.java
@@ -29,6 +29,21 @@ public class MahasiswaBerprestasi16 {
         return posisi;
     }
 
+    int findBinarySearch(double cari, int left, int right) {
+        int mid;
+        if (right >= left) {
+            mid = (left + right) / 2;
+            if (cari == listMhs[mid].ipk) {
+                return (mid);
+            } else if (listMhs[mid].ipk > cari) {
+                return findBinarySearch(cari, left, mid - 1);
+            } else {
+                return findBinarySearch(cari, mid + 1, right);
+            }
+        }
+        return -1;
+    }
+
     void tampilPosisi(double x, int pos) {
         if (pos != -1) {
             System.out.println("data mahasiswa dengan IPK : " + x + " ditemukan pada indeks " + pos);
